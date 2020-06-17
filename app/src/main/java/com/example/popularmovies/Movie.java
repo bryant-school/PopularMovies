3d930185e12c07e5759fbd86cb2625b610db1ec7
Bryant Johnson <bryantj@iguazu.T-mobile.com>
2020-06-16 22:53:55 -0500

    Movie: Implemented Parcelable
    This is needed so that we can pass this Object from one Activity to another as an extra.

--- app/src/main/java/com/example/popularmovies/Movie.java
+++ app/src/main/java/com/example/popularmovies/Movie.java
@@ -1,9 +1,13 @@
 package com.example.popularmovies;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.net.URL;
 import java.util.ArrayList;
 
-public class Movie {
+public class Movie implements Parcelable {
+
     private String mTitle;
     private String mReleaseDate;
     private String mMoviePoster;
@@ -16,10 +20,29 @@ public class Movie {
         mMoviePoster = moviePoster;
         mVoteAverage = voteAverage;
         mPlotSynopsis = plotSynopsis;
+    }
 
+    public Movie(Parcel parcel) {
+        mTitle = parcel.readString();
+        mReleaseDate = parcel.readString();
+        mMoviePoster = parcel.readString();
+        mVoteAverage = parcel.readInt();
+        mPlotSynopsis = parcel.readString();
     }
 
     private static int lastMovieId = 0;
+
+    public static final Creator<Movie> CREATOR = new Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel in) {
+            return new Movie(in);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
 
     public static ArrayList<Movie> CreateMovieList(int numMovies) {
         ArrayList<Movie> movies = new ArrayList<>();
@@ -49,5 +72,19 @@ public class Movie {
 
     public String getPlotSynopsis() {
         return mPlotSynopsis;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mTitle);
+        dest.writeString(mReleaseDate);
+        dest.writeString(mMoviePoster);
+        dest.writeInt(mVoteAverage);
+        dest.writeString(mPlotSynopsis);
     }
 }
